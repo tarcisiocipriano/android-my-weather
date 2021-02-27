fb25fb5194196fb9850a72f546b523c8a2890941
Tarcisio Cipriano <creativecipriano@gmail.com>
2021-02-27 11:07:24 -0300

    implement internet connection verification

---
+++ app/src/main/java/com/example/my_weather/extension/ContextExtension.kt
@@ -0,0 +1,21 @@
+package com.example.my_weather.extension
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+fun Context.isInternetAvailable(): Boolean {
+    var result = false
+
+    val cm = getSystemService(Context.CONNECTIVITY_SERVICE)
+            as ConnectivityManager
+
+    cm.getNetworkCapabilities(cm.activeNetwork)?.run {
+        result = when {
+            hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            else -> false
+        }
+    }
+    return result
+}
--- app/src/main/java/com/example/my_weather/ui/main/search/SearchFragment.kt
+++ app/src/main/java/com/example/my_weather/ui/main/search/SearchFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.my_weather.databinding.FragmentSearchBinding
+import com.example.my_weather.extension.isInternetAvailable
 
 class SearchFragment: Fragment() {
 
@@ -19,4 +21,16 @@ class SearchFragment: Fragment() {
         binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnSearch.setOnClickListener {
+            if (requireContext().isInternetAvailable()) {
+                Toast.makeText(requireContext(), "Has internet", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "No internet", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
